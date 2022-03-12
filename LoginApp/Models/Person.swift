24ad7f214ed08e2a858e6a81a5aecabfcc9435d6
Konstantin Fomenkov <kolij2005@gmail.com>
2022-03-12 12:57:03 +0300

    models

---
+++ LoginApp/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  LoginApp
+//
+//  Created by Konstantin Fomenkov on 12.03.2022.
+//
+
+import Foundation
---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Konstantin Fomenkov on 12.03.2022.
+//
+
+import Foundation
